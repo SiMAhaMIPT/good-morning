83b709df1b91ee57c9752b10a7081018975e8df7
Roman <roma11102004@yandex.ru>
2022-12-13 11:38:25 +0300

    Names are changes

--- inventory.py
+++ inventory.py
@@ -7,7 +7,7 @@ def ice():
 def glue():
     config.friction_coeff=0.9
     config.table_color =config.table_color_glue
-def standart_friction():
+def standart():
     config.friction_coeff=0.99
     config.table_color=config.table_color_standart
     config.cue_color = config.cue_color_standart
